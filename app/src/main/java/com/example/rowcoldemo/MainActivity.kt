8b292a145bb2887273cce5a70f18f276d2572dfc
tematrkv <tatarnikov.stud@mail.ru>
2025-09-19 16:20:55 +0700

    Дочерние элементы в конце области содержимого Row

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -51,9 +51,9 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Column(horizontalAlignment = Alignment.End,
-        modifier = modifier.width(250.dp)) {
-        TextCell("1")
+    Row(horizontalArrangement = Arrangement.End,
+        modifier = modifier.size(width = 400.dp, height = 200.dp)) {
+    TextCell("1")
         TextCell("2")
         TextCell("3")
     }
