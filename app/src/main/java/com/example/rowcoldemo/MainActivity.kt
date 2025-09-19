fafaefeefae5054961c0db7c7468517510689e36
tematrkv <tatarnikov.stud@mail.ru>
2025-09-19 16:18:59 +0700

    Элементы выровнены по краю области содержимого

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -51,13 +51,11 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        Row(verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier.size(width = 400.dp, height = 200.dp)) {
-            TextCell("1")
-            TextCell("2")
-            TextCell("3")
-        }
+    Column(horizontalAlignment = Alignment.End,
+        modifier = modifier.width(250.dp)) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
     }
 }
 
